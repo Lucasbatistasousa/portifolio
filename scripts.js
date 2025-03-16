7d3a21b82918731096e17bfeede4fbd0a157711f
Lucasbatistasousa <lucasbatistasousa2002@gmail.com>
2025-03-16 14:43:31 -0300

    Adição do video, configuração do menu lateral

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const menuButton = document.querySelector('#menu-button')
 const menu = document.querySelector('.div-header')
 const fecharMenu = document.querySelector('#fechar-menu')
 const fecharMenuMascara = document.querySelector('.mascara')
+const fecharMenuLink = document.querySelectorAll('.link-header')
+
 
 menuButton.addEventListener('click', function () {
     if (menu.style.display === 'none' && fecharMenu.style.display === 'none' && fecharMenuMascara.style.visibility === 'hidden') {
@@ -22,10 +24,21 @@ fecharMenu.addEventListener('click', function () {
         fecharMenuMascara.style.visibility = 'hidden'
     }
 })
+
 fecharMenuMascara.addEventListener('click', function () {
     if (menu.style.display === 'flex' && fecharMenu.style.display === 'inline') {
         menu.style.display = 'none'
         fecharMenu.style.display = 'none'
         fecharMenuMascara.style.visibility = 'hidden'
     }
+})
+
+document.querySelectorAll('.link-header').forEach(menulinks => {
+    menulinks.addEventListener('click', () => {
+        if (menu.style.display === 'flex' && fecharMenu.style.display === 'inline') {
+            menu.style.display = 'none'
+            fecharMenu.style.display = 'none'
+            fecharMenuMascara.style.visibility = 'hidden'
+        }
+    })
 })
